7f4f88fab751317ba4e3677471e5e86da8c096a0
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-23 22:51:50 +0200

    Global styles with styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles.js";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: var(--color-green-700);
+`
+
 function App() {
     return (
-        <div>
-            Hello World
-        </div>
+        <>
+            <GlobalStyles/>
+            <div>
+                <H1>The Wild Oasis</H1>
+            </div>
+        </>
     );
 }
 
