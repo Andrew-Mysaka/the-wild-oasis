c13b31b8acc1aa839d018dce9acf82772b82cf07
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-25 20:50:35 +0200

    setting up storage buckets

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -12,6 +12,7 @@ function Cabins() {
         <Row type="horizontal">
             <Heading as="h1">All cabins</Heading>
             <p>TEST</p>
+            <img src={'https://eczlrzibstkthjfgjxej.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2023-11-25T18%3A46%3A34.064Z'} alt={'Cabin #1'}/>
         </Row>
     );
 }
