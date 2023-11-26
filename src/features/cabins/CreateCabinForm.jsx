115aa4247d9fa7ba04645cc8e65bb0b717082682
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-26 17:04:28 +0200

    React Hook Form

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -5,6 +5,7 @@ import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
+import {useForm} from "react-hook-form";
 
 const FormRow = styled.div`
   display: grid;
@@ -43,47 +44,53 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
-  return (
-    <Form>
-      <FormRow>
-        <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" />
-      </FormRow>
+    const {register,handleSubmit} = useForm();
 
-      <FormRow>
-        <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" />
-      </FormRow>
+    function onSubmit(data){
+        console.log(data)
+    }
 
-      <FormRow>
-        <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" />
-      </FormRow>
+    return (
+        <Form onSubmit={handleSubmit(onSubmit)}>
+            <FormRow>
+                <Label htmlFor="name">Cabin name</Label>
+                <Input type="text" id="name" {...register("name")}/>
+            </FormRow>
 
-      <FormRow>
-        <Label htmlFor="discount">Discount</Label>
-        <Input type="number" id="discount" defaultValue={0} />
-      </FormRow>
+            <FormRow>
+                <Label htmlFor="maxCapacity">Maximum capacity</Label>
+                <Input type="number" id="maxCapacity" {...register("maxCapacity")}/>
+            </FormRow>
 
-      <FormRow>
-        <Label htmlFor="description">Description for website</Label>
-        <Textarea type="number" id="description" defaultValue="" />
-      </FormRow>
+            <FormRow>
+                <Label htmlFor="regularPrice">Regular price</Label>
+                <Input type="number" id="regularPrice" {...register("regularPrice")}/>
+            </FormRow>
 
-      <FormRow>
-        <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
-      </FormRow>
+            <FormRow>
+                <Label htmlFor="discount">Discount</Label>
+                <Input type="number" id="discount" defaultValue={0} {...register("discount")}/>
+            </FormRow>
 
-      <FormRow>
-        {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
-          Cancel
-        </Button>
-        <Button>Edit cabin</Button>
-      </FormRow>
-    </Form>
-  );
+            <FormRow>
+                <Label htmlFor="description">Description for website</Label>
+                <Textarea type="number" id="description" defaultValue="" {...register("description")}/>
+            </FormRow>
+
+            <FormRow>
+                <Label htmlFor="image">Cabin photo</Label>
+                <FileInput id="image" accept="image/*" />
+            </FormRow>
+
+            <FormRow>
+                {/* type is an HTML attribute! */}
+                <Button variation="secondary" type="reset">
+                    Cancel
+                </Button>
+                <Button>Edit cabin</Button>
+            </FormRow>
+        </Form>
+    );
 }
 
 export default CreateCabinForm;
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,12 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable.jsx";
+import Button from "../ui/Button.jsx";
+import {useState} from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm.jsx";
 
 function Cabins() {
+    const [showForm, setShowForm] = useState(false)
 
     return (
         <>
@@ -13,6 +17,9 @@ function Cabins() {
 
             <Row>
                 <CabinTable/>
+
+                <Button onClick={() => setShowForm(showForm => !showForm)}>Add new Cabin</Button>
+                {showForm && <CreateCabinForm/>}
             </Row>
         </>
     );
