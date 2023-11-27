42d58c13aee38e4c7c28ce6fac8c3ff8410139ae
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-27 20:22:10 +0200

    Duplicating Cabins

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -4,6 +4,10 @@ import {useState} from "react";
 import {formatCurrency} from "../../utils/helpers.js";
 import CreateCabinForm from "./CreateCabinForm.jsx";
 import {useDeleteCabin} from "./useDeleteCabin.js";
+import {HiSquare2Stack} from "react-icons/hi2";
+import {HiPencil, HiTrash} from "react-icons/hi";
+import {useCabins} from "./useCabins.js";
+import {useCreateCabin} from "./useCreateCabin.js";
 
 
 const TableRow = styled.div`
@@ -48,8 +52,20 @@ const Discount = styled.div`
 function CabinRow({cabin}) {
     const [showForm, setShowForm] = useState(false)
     const {isDeleting, deleteCabin} = useDeleteCabin()
+    const {isCreating, createCabin} = useCreateCabin();
 
-    const {id: cabinId, name, maxCapacity, regularPrice, discount, image} = cabin;
+    const {id: cabinId, name, maxCapacity, regularPrice, discount, image, description} = cabin;
+
+    function handleDuplicate() {
+        createCabin({
+            name: `Copy of ${name}`,
+            maxCapacity,
+            regularPrice,
+            discount,
+            image,
+            description
+        })
+    }
 
     return (
         <>
@@ -60,8 +76,9 @@ function CabinRow({cabin}) {
                 <Price>{formatCurrency(regularPrice)}</Price>
                 {discount ? <Discount>{formatCurrency(discount)}</Discount> : <span>&mdash;</span>}
                 <div>
-                    <button onClick={() => setShowForm(show => !show)}>Edit</button>
-                    <button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}>Delete</button>
+                    <button onClick={handleDuplicate} disabled={isCreating}><HiSquare2Stack/></button>
+                    <button onClick={() => setShowForm(show => !show)}><HiPencil/></button>
+                    <button onClick={() => deleteCabin(cabinId)} disabled={isDeleting}><HiTrash/></button>
                 </div>
             </TableRow>
 
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -44,6 +44,8 @@ export async function createEditCabin(newCabin, id){
     }
 
     // 2. Upload an image
+    if(hasImagePath) return data;
+
     const { error: storageError } = await supabase
         .storage
         .from("cabin-images")
