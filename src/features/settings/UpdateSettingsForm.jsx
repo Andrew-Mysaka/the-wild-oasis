9eb7c1390c3c4366b1acc311296c35d26564d4f6
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-11-27 21:33:35 +0200

    Fetching Applications Settings

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,24 +1,30 @@
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
+import {useSettings} from "./useSettings.js";
+import Spinner from "../../ui/Spinner.jsx";
 
 function UpdateSettingsForm() {
-  return (
-    <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
-      </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
-      </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
-      </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
-      </FormRow>
-    </Form>
-  );
+   const {isLoading, settings: {minBookingLength, maxBookingLength, maxGuestsPerBooking, breakfastPrice} = {}} = useSettings();
+
+   if(isLoading) return <Spinner/>
+
+    return (
+        <Form>
+            <FormRow label='Minimum nights/booking'>
+                <Input type='number' id='min-nights' defaultValue={minBookingLength}/>
+            </FormRow>
+            <FormRow label='Maximum nights/booking'>
+                <Input type='number' id='max-nights' defaultValue={maxBookingLength}/>
+            </FormRow>
+            <FormRow label='Maximum guests/booking'>
+                <Input type='number' id='max-guests' defaultValue={maxGuestsPerBooking}/>
+            </FormRow>
+            <FormRow label='Breakfast price'>
+                <Input type='number' id='breakfast-price' defaultValue={breakfastPrice}/>
+            </FormRow>
+        </Form>
+    );
 }
 
 export default UpdateSettingsForm;
