b791fa86826488f3abbe39b21ce528144e7da457
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-12-01 21:40:02 +0200

    building a Reusable Table

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -8,19 +8,20 @@ import {HiPencil, HiTrash} from "react-icons/hi";
 import {useCreateCabin} from "./useCreateCabin.js";
 import Modal from "../../ui/Modal.jsx";
 import ConfirmDelete from "../../ui/ConfirmDelete.jsx";
+import Table from "../../ui/Table.jsx";
 
 
-const TableRow = styled.div`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-  padding: 1.4rem 2.4rem;
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-`;
+// const TableRow = styled.div`
+//   display: grid;
+//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//   column-gap: 2.4rem;
+//   align-items: center;
+//   padding: 1.4rem 2.4rem;
+//
+//   &:not(:last-child) {
+//     border-bottom: 1px solid var(--color-grey-100);
+//   }
+// `;
 
 const Img = styled.img`
   display: block;
@@ -67,7 +68,7 @@ function CabinRow({cabin}) {
     }
 
     return (
-        <TableRow role={'row'}>
+        <Table.Row>
             <Img src={image}/>
             <Cabin>{name}</Cabin>
             <div>Fits up to {maxCapacity} guests</div>
@@ -83,10 +84,10 @@ function CabinRow({cabin}) {
                         <CreateCabinForm cabinToEdit={cabin}/>
                     </Modal.Window>
 
-                    <Modal.Open>
+                    <Modal.Open opens={'delete'}>
                         <button><HiTrash/></button>
                     </Modal.Open>
-                    <Modal.Window>
+                    <Modal.Window name={"delete"}>
                         <ConfirmDelete
                             resourceName={'cabins'}
                             disabled={isDeleting}
@@ -95,7 +96,7 @@ function CabinRow({cabin}) {
                     </Modal.Window>
                 </Modal>
             </div>
-        </TableRow>
+        </Table.Row>
     );
 }
 
--- src/features/cabins/CabinTable-v1.jsx
+++ src/features/cabins/CabinTable-v1.jsx
@@ -2,7 +2,15 @@ import styled from "styled-components";
 import Spinner from "../../ui/Spinner.jsx";
 import CabinRow from "./CabinRow.jsx";
 import {useCabins} from "./useCabins.js";
-import Table from "../../ui/Table.jsx";
+
+const Table = styled.div`
+  border: 1px solid var(--color-grey-200);
+
+  font-size: 1.4rem;
+  background-color: var(--color-grey-0);
+  border-radius: 7px;
+  overflow: hidden;
+`;
 
 const TableHeader = styled.header`
   display: grid;
@@ -25,15 +33,15 @@ function CabinTable() {
     if(isLoading) return <Spinner/>
 
     return (
-        <Table columns={"0.6fr 1.8fr 2.2fr 1fr 1fr 1fr"}>
-            <Table.Header role="row">
+        <Table role="table">
+            <TableHeader role="row">
                 <div></div>
                 <div>Cabin</div>
                 <div>Capacity</div>
                 <div>Price</div>
                 <div>Discount</div>
                 <div></div>
-            </Table.Header>
+            </TableHeader>
             {cabins.map((cabin) => (
                 <CabinRow cabin={cabin} key={cabin.id}/>
                 ))}
