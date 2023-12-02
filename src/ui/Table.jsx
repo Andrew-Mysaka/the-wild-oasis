d029a018404e1887f2cf545b8bb7105e3a9aab76
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-12-02 15:24:47 +0200

    Applying the Render Props Pattern

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -86,8 +86,12 @@ function Row({children}){
     </StyledRow>
 }
 
-function Body({children}){
+function Body({data, render}){
+    if(!data.length) return <Empty>No data to show at the moment</Empty>
 
+    return <StyledBody>
+        {data.map(render)}
+    </StyledBody>
 }
 
 Table.Header = Header
