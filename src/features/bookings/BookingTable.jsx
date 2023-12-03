8ffe968b5ad04032131b23a538503fea74c6267d
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-12-03 14:41:36 +0200

    Building a Reusable Pagination Component

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -4,6 +4,7 @@ import Menus from "../../ui/Menus";
 import Empty from "../../ui/Empty.jsx";
 import {useBookings} from "./useBookings.js";
 import Spinner from "../../ui/Spinner.jsx";
+import Pagination from "../../ui/Pagination.jsx";
 
 function BookingTable() {
     const {bookings, isLoading} = useBookings()
@@ -30,6 +31,10 @@ function BookingTable() {
                         <BookingRow key={booking.id} booking={booking}/>
                     )}
                 />
+
+                <Table.Footer>
+                    <Pagination count={15}/>
+                </Table.Footer>
             </Table>
         </Menus>
     );
