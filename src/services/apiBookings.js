cbcc37351551ce2557f448ce739ad0cb688f5f9b
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-12-03 12:58:31 +0200

    API-Side Sorting: Sorting Bookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -7,7 +7,10 @@ export async function getBookings({filter, sortBy}) {
         .select("id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)")
 
     // FILTER
-    if(filter !== null) query = query[filter.method || "eq"](filter.field, filter.value)
+    if(filter) query = query[filter.method || "eq"](filter.field, filter.value)
+
+    // SORT
+    if(sortBy) query = query.order(sortBy.field, {ascending: sortBy.direction === "asc"})
 
     const {data, error} = await query;
 
