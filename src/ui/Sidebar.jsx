f6609ac1c4c3f4ab372681b52c81eab3a7016413
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-12-03 11:53:35 +0200

    Uploading Sample Data

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 import Logo from "./Logo.jsx";
 import MainNav from "./MainNav.jsx";
+import Uploader from "../data/Uploader.jsx";
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
-  
+
   grid-row: 1 / -1;
   display: flex;
   flex-direction: column;
@@ -18,6 +19,8 @@ function Sidebar() {
         <StyledSidebar>
             <Logo/>
             <MainNav/>
+
+            <Uploader/>
         </StyledSidebar>
     );
 }
