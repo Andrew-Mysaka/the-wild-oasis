44df6d87c1f6f06d312f1eb11c119c3854db6fb2
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-12-03 12:38:48 +0200

    API-Side Filtering: Filtering Bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,10 +1,21 @@
 import {useQuery} from "@tanstack/react-query";
 import {getBookings} from "../../services/apiBookings.js";
+import {useSearchParams} from "react-router-dom";
 
 export function useBookings(){
+    const [searchParams] = useSearchParams()
+
+    // FILTER
+    const filterValue = searchParams.get("status");
+    const filter =
+        !filterValue || filterValue === "all"
+            ? null
+            : {field: "status", value: filterValue};
+            // : {field: "totalPrice", value: 5000, method: "gte"};
+
     const {isLoading, data: bookings, error} = useQuery({
-        queryKey: ['bookings'],
-        queryFn: getBookings
+        queryKey: ['bookings', filter],
+        queryFn: () => getBookings({filter})
     })
 
     return {isLoading, bookings, error}
--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,13 +1,14 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import BookingTable from "../features/bookings/BookingTable.jsx";
+import BookingTableOperations from "../features/bookings/BookingTableOperations.jsx";
 
 function Bookings() {
     return (
         <>
             <Row type="horizontal">
                 <Heading as="h1">All bookings</Heading>
-                <p>TEST</p>
+                <BookingTableOperations/>
             </Row>
 
             <BookingTable/>
