ec70a6a8f1b587c0e87a4441a865decf5ab7c2c1
Andrew-Mysaka <andrewmysaka@gmail.com>
2023-12-09 19:42:43 +0200

    Updating User Data and Password

---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,21 @@
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import {toast} from "react-hot-toast";
+import {updateCurrentUser} from "../../services/apiAuth.js";
+
+export function useUpdateUser(){
+    const queryClient = useQueryClient();
+
+    const {mutate: updateUser, isLoading: isUpdating } = useMutation({
+        mutationFn: updateCurrentUser,
+        onSuccess: (user) => {
+            toast.success("User account successfully updated")
+            // queryClient.setQueryData("user", user)
+            queryClient.invalidateQueries({queryKey: ['user']})
+        },
+        onError: error => {
+            toast.error(error.message)
+        }
+    })
+
+    return {isUpdating, updateUser}
+}
